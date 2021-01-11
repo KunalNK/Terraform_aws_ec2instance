f7fcac7fb47d5e553634562cf7291763626aefff
Kunal K <knkillekar@gmail.com>
2021-01-11 18:29:56 +0530

    updated script

--- main.tf
+++ main.tf
@@ -135,7 +135,7 @@ resource "aws_instance" "web-server-instance" {
   instance_type     = "t2.micro"
   availability_zone = "us-east-1a"
   key_name          = "ansible"
-
+  
   network_interface {
     device_index         = 0
     network_interface_id = aws_network_interface.web-server-nic.id
